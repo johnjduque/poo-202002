07702d611575d2e64b2d6a17df47c970e6d6c9ce
Alejandro Ramírez <alejandroramirez@saimyr.com.co>
2020-08-14 19:23:07 -0500

    Se crean varios televisores y se hace foreach

--- src/com/alejandro/app/AppTelevisores.java
+++ src/com/alejandro/app/AppTelevisores.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 public class AppTelevisores {
     public static void main(String[] args) {
-        Televisor tv = new Televisor();
-
 //        short variableControl = 1;
 //
 //        do {
@@ -23,9 +21,29 @@ public class AppTelevisores {
 
         numeros.forEach(x -> System.out.println("El valor es de " + x));
 
+        // TODO Explorar todo lo que tenga Televisor y jugar con la clase ArrayList
+
+        Televisor tv = new Televisor();
+        tv.marca = "Samsung";
+        tv.prender();
+
+        Televisor tv2 = new Televisor();
+        tv2.marca = "Sony";
+        tv2.prender();
+
+        Televisor tv3 = new Televisor();
+        tv3.marca = "Challenger";
+        tv3.apagar();
+
         ArrayList<Televisor> televisores = new ArrayList<>();
         televisores.add(tv);
+        televisores.add(tv2);
+        televisores.add(tv3);
 
-        // TODO Explorar todo lo que tenga Televisor y jugar con la clase ArrayList
+        televisores.forEach(televisor -> {
+            // TODO Reemplazar el boolean por el texto en cuestión "Apagado" ó "Prendido"
+            System.out.println(televisor.marca + " está " + televisor.prendido);
+        });
+
     }
 }
